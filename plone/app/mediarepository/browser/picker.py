2542f1218fcc7d0b5cb3e288f8433d251ccaa08a
Jamie Lentin <jm@lentin.co.uk>
2011-09-21 21:11:43 +0100

    Add picker view

---
+++ plone/app/mediarepository/browser/picker.py
@@ -0,0 +1,7 @@
+from Products.Five import BrowserView
+from zope.component import getUtility
+from zope.app.intid.interfaces import IIntIds
+
+class Picker(BrowserView):
+    def __init__(self, context, request):
+        BrowserView.__init__(self, context, request)
