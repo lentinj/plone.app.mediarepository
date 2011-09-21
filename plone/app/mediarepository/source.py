c6baca4f7b2aae48cb79a60866890671927b8368
Jamie Lentin <jm@lentin.co.uk>
2011-09-21 21:12:58 +0100

    Add a source vocabulary for the image tile form to use

---
+++ plone/app/mediarepository/source.py
@@ -0,0 +1,17 @@
+from plone.formwidget.contenttree import UUIDSourceBinder
+
+from zope.app.component.hooks import getSite
+
+class MediaRepoSourceBinder(UUIDSourceBinder):
+    def __init__(self):
+        # Ideally would set navigation tree query at this point, but don't seem
+        # to be able to get a site. Wait until call
+        super(MediaRepoSourceBinder, self).__init__()
+    
+    def __call__(self, context):
+        site = getSite()
+        repo = site.restrictedTraverse('media-repository')
+        repoPath = '/'.join(repo.getPhysicalPath())
+        self.navigation_tree_query = {'path': {'query': repoPath}}
+        source = super(MediaRepoSourceBinder, self).__call__(context)
+        return source
