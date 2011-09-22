b5a308e61a4edc846d9512133eeded08a9eb9791
Ralph Jacobs <ralph@fourdigits.nl>
2011-09-22 12:16:11 +0200

    PEP8

--- plone/app/mediarepository/browser/picker.py
+++ plone/app/mediarepository/browser/picker.py
@@ -2,6 +2,7 @@ from Products.Five import BrowserView
 from zope.component import getUtility
 from zope.app.intid.interfaces import IIntIds
 
+
 class Picker(BrowserView):
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
