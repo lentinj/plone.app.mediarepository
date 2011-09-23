db836625d28a78f3587646c54da4a1cb2b582074
Jamie Lentin <jm@lentin.co.uk>
2011-09-23 13:25:43 +0100

    Add uploader container

--- plone/app/mediarepository/browser/picker.py
+++ plone/app/mediarepository/browser/picker.py
@@ -3,4 +3,12 @@ from Products.Five import BrowserView
 
 class Picker(BrowserView):
     def __init__(self, context, request):
+        self.context = context
         BrowserView.__init__(self, context, request)
+    
+    def getUploadUrl(self):
+        folder_url = self.context['media-repository'].absolute_url()
+        return '%s/@@quick_upload' %folder_url
+    
+    def getDataForUploadUrl(self):
+        return 'data_url'
